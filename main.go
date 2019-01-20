63982ca1a1707ee293eb5fd38625caa2c42e6f04
Adrian Gheorghe <adrian.gheorghe.dev@gmail.com>
2019-01-20 18:00:25 +0000

    Update command to use shell present on system

--- main.go
+++ main.go
@@ -21,18 +21,18 @@ func (i *arrayFlags) Set(value string) error {
 	return nil
 }
 
-func waitFor(waitsFlags arrayFlags, commandFlags arrayFlags, timeoutFlag int, intervalFlag int) {
+func waitFor(waitsFlags arrayFlags, commandFlags arrayFlags, timeoutFlag int, intervalFlag int, shell string) {
 	for _, wait := range waitsFlags {
-		processWait(wait, timeoutFlag, intervalFlag)
+		processWait(wait, timeoutFlag, intervalFlag, shell)
 	}
 
 	for _, command := range commandFlags {
-		processCommandExec(command, timeoutFlag, intervalFlag)
+		processCommandExec(command, timeoutFlag, intervalFlag, shell)
 	}
 
 }
 
-func processWait(wait string, timeoutFlag int, intervalFlag int) {
+func processWait(wait string, timeoutFlag int, intervalFlag int, shell string) {
 	pattern, _ := regexp.Compile("(.*):(.*)")
 	matches := pattern.FindAllStringSubmatch(wait, -1)
 	if len(matches) > 0 {
@@ -53,7 +53,7 @@ func processWait(wait string, timeoutFlag int, intervalFlag int) {
 		}
 	} else {
 		for {
-			out, err := exec.Command("bash", "-c", wait).Output()
+			out, err := exec.Command(shell, "-c", wait).Output()
 			if err != nil {
 				fmt.Printf("Sleeping %d seconds waiting for command - %s - to return\n", intervalFlag, wait)
 				time.Sleep(time.Duration(intervalFlag) * time.Second)
@@ -66,13 +66,31 @@ func processWait(wait string, timeoutFlag int, intervalFlag int) {
 	}
 }
 
-func processCommandExec(command string, timeoutFlag int, intervalFlag int) {
-	out, err := exec.Command("bash", "-c", command).Output()
+func processCommandExec(command string, timeoutFlag int, intervalFlag int, shell string) {
+	out, err := exec.Command(shell, "-c", command).Output()
 	if err != nil {
 		fmt.Printf("Sleeping %d seconds waiting for command - %s - to return\n", intervalFlag, command)
 		time.Sleep(time.Duration(intervalFlag) * time.Second)
 	} else {
 		fmt.Println(string(out))
+	}
+}
+
+func inPath(command string) bool {
+	_, err := exec.LookPath(command)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func chooseShell() string {
+	if inPath("bash") {
+		return "bash"
+	} else if inPath("sh") {
+		return "sh"
+	} else {
+		panic("Neither bash or sh present on system")
 	}
 }
 
@@ -83,7 +101,7 @@ func main() {
 	flag.Var(&waitsFlags, "wait", "You can specify the HOST and TCP PORT using the format HOST:PORT, or you can specify a command that should return an output. Multiple wait flags can be added.")
 	flag.Var(&commandFlags, "command", "Command that should be run when all waits are accessible. Multiple commands can be added.")
 	timeoutFlag := flag.Int("timeout", 600, "Timeout untill script is killed.")
-	intervalFlag := flag.Int("interval", 5, "Interval between calls")
+	intervalFlag := flag.Int("interval", 15, "Interval between calls")
 	flag.Parse()
 
 	if len(waitsFlags) == 0 || len(commandFlags) == 0 {
@@ -91,5 +109,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	waitFor(waitsFlags, commandFlags, *timeoutFlag, *intervalFlag)
+	shell := chooseShell()
+	waitFor(waitsFlags, commandFlags, *timeoutFlag, *intervalFlag, shell)
 }
